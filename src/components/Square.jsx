4b21b2a1b0e9d8f8a0d06dba20f4f3b3c34a3aaa
JungyeonShin <feelsagood@naver.com>
2024-01-07 22:39:55 +0900

    first commit

---
+++ src/components/Square.jsx
@@ -0,0 +1,24 @@
+import { Component } from "react";
+import "./Square.css";
+
+export class Square extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: null,
+    };
+  }
+
+  render() {
+    return (
+      <button
+        className="square"
+        onClick={() => {
+          this.setState({ value: "X" });
+        }}
+      >
+        {this.state.value}
+      </button>
+    );
+  }
+}
