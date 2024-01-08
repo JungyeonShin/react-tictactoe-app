f5bcc545cab6e8498c8e75f00a04a26f3b456ed6
JungyeonShin <feelsagood@naver.com>
2024-01-08 22:57:50 +0900

    게임결과 처리 완료

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,32 +1,83 @@
-import { Component } from "react";
-import { Square } from "./Square";
+import { useState } from "react";
+import Square from "./Square";
 import "./Board.css";
 
-export class Board extends Component {
-  renderSquare(i) {
-    return <Square value={i} />;
+const Board = () => {
+  const [squares, setSquares] = useState(Array(9).fill(null));
+  const [xIsNext, setXIsNext] = useState(true);
+
+  const calculateWinner = (squares) => {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+
+    for (let index = 0; index < lines.length; index++) {
+      const [a, b, c] = lines[index];
+
+      if (
+        squares[a] &&
+        squares[a] === squares[b] &&
+        squares[a] === squares[c]
+      ) {
+        return squares[a];
+      }
+    }
+
+    return null;
+  };
+
+  const winner = calculateWinner(squares);
+  let status;
+  if (winner) {
+    status = "Winner: " + winner;
+  } else {
+    status = `Next Player ${xIsNext ? "X" : "O"}`;
   }
 
-  render() {
-    return (
-      <div>
-        <div className="status">Next Player: X, O</div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
+  const handleClick = (i) => {
+    const newSquares = squares.slice();
+
+    if (calculateWinner(newSquares) || newSquares[i]) {
+      return;
+    }
+
+    newSquares[i] = xIsNext ? "X" : "O";
+    setSquares(newSquares);
+    setXIsNext((prev) => !prev);
+  };
+
+  const renderSquare = (i) => {
+    console.log(squares[i]);
+    return <Square value={squares[i]} onClick={() => handleClick(i)} />;
+  };
+
+  return (
+    <div>
+      <div className="status">{status}</div>
+      <div className="board-row">
+        {renderSquare(0)}
+        {renderSquare(1)}
+        {renderSquare(2)}
       </div>
-    );
-  }
-}
+      <div className="board-row">
+        {renderSquare(3)}
+        {renderSquare(4)}
+        {renderSquare(5)}
+      </div>
+      <div className="board-row">
+        {renderSquare(6)}
+        {renderSquare(7)}
+        {renderSquare(8)}
+      </div>
+    </div>
+  );
+};
+
+export default Board;
